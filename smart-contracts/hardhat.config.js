7b7c9ee70410ec412b7d9c41aa34275b45966231
Meriem-BM <barhoumi.meriem1@gmail.com>
2024-12-22 03:34:42 +0100

    feat: update hardhat config to testnet

--- smart-contracts/hardhat.config.js
+++ smart-contracts/hardhat.config.js
@@ -1,11 +1,13 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
-  // networks: {
-  //   sepolia: {
-  //     url: "https://worldchain-sepolia.g.alchemy.com/v2/V0HTAoXOb259UegcW1YpTafSVldNsrXd",
-  //   },
-  // },
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_RPC_API_KEY}`,
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY],
+    },
+  },
 };
