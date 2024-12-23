d4274fd564dbe2793e6ff24fa767ca26f6447301
Meriem-BM <barhoumi.meriem1@gmail.com>
2024-12-23 01:06:59 +0100

    add wallet connect project ID

--- client/src/providers/WagmiProvider.tsx
+++ client/src/providers/WagmiProvider.tsx
@@ -4,19 +4,23 @@ import { createWeb3Modal } from "@web3modal/wagmi/react";
 import { WagmiProvider } from "wagmi";
 import { wagmiConfig, projectId, metadata } from "@/wagmiConfigs";
 
+if (!projectId) {
+  throw new Error("Missing WALLET_CONNECT_ID");
+}
+
 createWeb3Modal({
-	wagmiConfig,
-	projectId,
-	metadata,
-	themeMode: "light",
-	allowUnsupportedChain: false,
-	enableAnalytics: true,
+  wagmiConfig,
+  projectId,
+  metadata,
+  themeMode: "light",
+  allowUnsupportedChain: false,
+  enableAnalytics: true,
 });
 
 export default function WagmiWrapper({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return <WagmiProvider config={wagmiConfig}>{children}</WagmiProvider>;
+  return <WagmiProvider config={wagmiConfig}>{children}</WagmiProvider>;
 }
--- client/src/wagmiConfigs.ts
+++ client/src/wagmiConfigs.ts
@@ -1,31 +1,31 @@
 import { cookieStorage, createConfig, createStorage } from "wagmi";
 import { createClient, http } from "viem";
 import {
-	// arbitrum,
-	// base,
-	// gnosis,
-	// mainnet,
-	// optimism,
-	// polygon,
-	sepolia,
+  // arbitrum,
+  // base,
+  // gnosis,
+  // mainnet,
+  // optimism,
+  // polygon,
+  sepolia,
 } from "wagmi/chains";
 
-export const projectId = "275cb569b0d41a83cb3e9ebc1250cb68";
+export const projectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_ID;
 
 export const metadata = {
-	name: "Pro Palestinian Donation",
-	description: "Donate to the Palestinian cause",
-	url: "",
-	icons: [],
+  name: "ProPal",
+  description: "Donate to the Palestinian cause",
+  url: "",
+  icons: [],
 };
 
 export const wagmiConfig = createConfig({
-	chains: [sepolia],
-	ssr: true,
-	storage: createStorage({
-		storage: cookieStorage,
-	}),
-	client({ chain }) {
-		return createClient({ chain, transport: http() });
-	},
+  chains: [sepolia],
+  ssr: true,
+  storage: createStorage({
+    storage: cookieStorage,
+  }),
+  client({ chain }) {
+    return createClient({ chain, transport: http() });
+  },
 });
